bbb4b60c233c2f102de964f1cc9c4b5774878f68
indyuriii <indyuriii08@gmail.com>
2025-04-10 13:44:41 +0800

    finally backend is online on pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,10 @@ module.exports = {
     apps: [
       {
         name: "strapi-backend",
-        script: "npm",
+        script: "cmd.exe",
         args: "run develop", // For development mode; later change to a production command if needed
+        interpreter: "none",  
+        cwd: "C:\\Users\\User-Desktop\\Desktop\\PMS\\pms_backend", // Ensure this is the correct folder
         env: {
           NODE_ENV: "development"
         },
